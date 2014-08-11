a9c1b64807d106f0254ab2b4be7cf2c3030a71be
eshubkagel <eshubkagel@gmail.com>
2014-08-11 00:11:57 -0500

    Initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+
+using ShubkagelApp;
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.GKey = "AIzaSyB1PwaN07ox0oOxoQ3dP7OHbLhYFMPVgfQ";
+            return View("Index");
+        }
+
+     
+    }
+}
